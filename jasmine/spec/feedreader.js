2383e61bf039a3fa25778e2c3ab49bc6376e6990
Valer-Muresan <muresanvaler86@yahoo.com>
2019-11-25 17:40:08 +0200

    update the jasmine version

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,7 +73,7 @@ $(function() {
           it('display menu when is clicked and hide menu when is clicked again', () => {
             let menuIcon = $('.menu-icon-link');
             menuIcon.click();
-            expect($('body').hasClass('menu-hidden')).not.toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(false);
             menuIcon.click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
           });
